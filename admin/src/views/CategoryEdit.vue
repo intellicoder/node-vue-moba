74e561c1d47146f9fed78f9637655f3c651a7237
zhuzhi <3300945918@qq.com>
2021-05-02 22:51:49 +0800

    elementui初始化你

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>新建分类</h1>
+        <el-form label-width="120px">
+            <el-form-item label="名称">
+                <el-input v-model="model.name" @submit.native.prevent="save"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" native-type="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: {}
+        }
+    },
+    methods: {
+        save() {
+            this.$http.post(`路径`, {})
+        }
+    },
+}
+</script>
