e3bfea2fd5c592abe48f78781e2131aeb327638f
zhuzhi <3300945918@qq.com>
2021-05-13 17:55:15 +0800

    富文本编辑器

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,8 +4,15 @@ import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
+
 import ItemEdit from '../views/ItemEdit.vue'
 import ItemList from '../views/ItemList.vue'
+
+import HeroEdit from '../views/HeroEdit.vue'
+import HeroList from '../views/HeroList.vue'
+
+import ArticleEdit from '../views/ArticleEdit.vue'
+import ArticleList from '../views/ArticleList.vue'
 
 Vue.use(VueRouter)
 
@@ -18,9 +25,18 @@ const routes = [
       { path: '/categories/create',component: CategoryEdit },
       { path: '/categories/list', component: CategoryList },
       { path: '/categories/edit/:id', component: CategoryEdit, props: true},
+
       { path: '/items/create',component: ItemEdit },
       { path: '/items/list', component: ItemList },
       { path: '/items/edit/:id', component: ItemEdit, props: true},
+
+      { path: '/heroes/create',component: HeroEdit },
+      { path: '/heroes/list', component: HeroList },
+      { path: '/heroes/edit/:id', component: HeroEdit, props: true},
+
+      { path: '/articles/create',component: ArticleEdit },
+      { path: '/articles/list', component: ArticleList },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true},
     
     ]
   },
